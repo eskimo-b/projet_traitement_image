02fd3ce5012d94e99bd355e8f5bbdc86b7ca3316
CHAHMA Nael <Nael.Chahma@bordeaux-inp.fr>
2025-11-28 11:20:55 +0100

    homography_proj fonctionnelle

--- homography_projection.py
+++ homography_projection.py
@@ -1,6 +1,6 @@
 import numpy as np
+from PIL import Image
 import matplotlib.pyplot as plt
-
 
 def  homography_estimate(x1, y1, x2, y2):
     A = np.zeros((8,8))
@@ -37,22 +37,53 @@ def homography_apply(H,x1,y1):
 
 def homography_projection(I1, I2, x, y):
     h1,w1 = I1.shape
+    h2, w2 = I2.shape
     x1 = np.array([0,w1-1,w1-1,0])
     y1 = np.array([0,0,h1-1,h1-1])
-    h2,w2 = y[2] + 1, x[1] + 1
     H = homography_estimate(x,y,x1,y1)
     for j in range(h2): #y
         for i in range(w2): #x
             xs, ys = homography_apply(H, i, j)
-            xs = np.round(xs)
-            ys = np.round(ys)
-            if ((0<= xs <= w1) and (0 <= ys <= h1)):
+            xs = int(np.round(xs))
+            ys = int(np.round(ys))
+            if ((0<= xs < w1) and (0 <= ys < h1)):
                 I2[j, i] = I1[ys, xs]
     return I2
 
-I1 = plt.imread('img/barbara.bmp')
-I2 = plt.imread('img/cameraman.tif')
-x, y = 
+points = []
+
+# fonction faite par IA
+
+def onclick(event):
+    if event.xdata is None or event.ydata is None:
+        return
+
+    x = int(event.xdata)
+    y = int(event.ydata)
+
+    points.append([x, y])
+    
+    plt.plot(event.xdata, event.ydata, "ro")
+    plt.draw()
+    
+    if len(points) == 4:
+       plt.close()
+
+    print("\nPoints sélectionnés :", points)
+
+
+I1 = Image.open('./img/barbara.bmp')
+I1 = np.array(I1)
+plt.figure("Sélection des 4 coins")
+
+I2 = Image.open('./img/cameraman.tif')
+I2 = np.array(I2)
+plt.imshow(I2)
+cid = plt.gcf().canvas.mpl_connect("button_press_event", onclick)
+plt.show(block=True)
+
+x = [points[k][0] for k in range(len(points))]
+y = [points[k][1] for k in range(len(points))]
 
 I2 = homography_projection(I1, I2, x, y)
 
